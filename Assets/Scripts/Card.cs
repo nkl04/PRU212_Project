a5ee19a13138980ef37750f34f901dd6f4efebc2
nkl04 <longnguyenkim2707@gmail.com>
2025-01-26 00:12:45 +0700

    feat: base card drag

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler
 {
+
+    [SerializeField] private bool instantiateVisual = true;
+    private Canvas canvas;
+    private Image imageComponent;
+    private Vector3 offset;
+
     [Header("Movement")]
     [SerializeField] private float moveSpeedLimit = 50f;
 
@@ -12,39 +20,97 @@ public class Card : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHand
     private float pointerDownTime;
     private float pointerUpTime;
 
+    [Header("States")]
+    public bool isDragging = false;
+    public bool isHovering = false;
+
+    [HideInInspector] private bool wasDragged = false;
+
+    [Header("Events")]
+    [HideInInspector] public UnityEvent<Card> PointerEnterEvent;
+    [HideInInspector] public UnityEvent<Card> PointerExitEvent;
+    [HideInInspector] public UnityEvent<Card, bool> PointerUpEvent;
+    [HideInInspector] public UnityEvent<Card> PointerDownEvent;
+    [HideInInspector] public UnityEvent<Card> BeginDragEvent;
+    [HideInInspector] public UnityEvent<Card> EndDragEvent;
+    [HideInInspector] public UnityEvent<Card, bool> SelectEvent;
+
+    private void Start()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        imageComponent = GetComponent<Image>();
+
+    }
+
+    private void Update()
+    {
+        ClampPosition();
+
+        if (isDragging)
+        {
+            Vector2 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - offset;
+
+            Vector2 direction = (targetPosition - (Vector2)transform.position).normalized;
+
+            Vector2 velocity = direction * Mathf.Min(moveSpeedLimit, Vector2.Distance(transform.position, targetPosition) / Time.deltaTime);
+
+            transform.Translate(velocity * Time.deltaTime);
+        }
+    }
+
+    private void ClampPosition()
+    {
+        Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        Vector3 clampedPosition = transform.position;
+
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -screenBounds.x, screenBounds.x);
+
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -screenBounds.y, screenBounds.y);
+
+        transform.position = new Vector3(clampedPosition.x, clampedPosition.y, 0);
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        // on drag
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        BeginDragEvent.Invoke(this);    // Invoke the event
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        offset = mousePosition - (Vector2)transform.position;
+
+        isDragging = true;
+
+        canvas.GetComponent<GraphicRaycaster>().enabled = false; // Disable raycast target to allow raycast to pass through the card
+
+        imageComponent.raycastTarget = false;
+
+        wasDragged = true;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
     }
 
 }
