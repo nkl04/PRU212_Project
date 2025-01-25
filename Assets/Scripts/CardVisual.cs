bb94c72c2d8b03cf0d3b87eed66e2d7ee1bf60fa
nkl04 <longnguyenkim2707@gmail.com>
2025-01-26 01:06:40 +0700

    feat: base cardvisual

---
+++ Assets/Scripts/CardVisual.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class CardVisual : MonoBehaviour
+{
+    private bool initalize = false;
+
+    [Header("Card")]
+    public Card parentCard;
+    private Transform cardTransform;
+    private Canvas canvas;
+
+    [Header("References")]
+    public Transform visualShadow;
+    private Canvas shadowCanvas;
+
+    private void Start()
+    {
+
+    }
+
+    private void Initalize(Card card, int index = 0)
+    {
+        parentCard = card;
+        cardTransform = card.transform;
+        canvas = GetComponentInParent<Canvas>();
+        shadowCanvas = visualShadow.GetComponent<Canvas>();
+
+        parentCard.BeginDragEvent.AddListener(OnBeginDrag);
+        parentCard.EndDragEvent.AddListener(OnEndDrag);
+        parentCard.PointerEnterEvent.AddListener(OnPointerEnter);
+        parentCard.PointerExitEvent.AddListener(OnPointerExit);
+        parentCard.PointerUpEvent.AddListener(OnPointerUp);
+        parentCard.PointerDownEvent.AddListener(OnPointerDown);
+        parentCard.SelectEvent.AddListener(OnSelect);
+
+        initalize = true;
+    }
+
+    private void OnBeginDrag(Card card)
+    {
+        // on begin drag
+    }
+
+    private void OnEndDrag(Card card)
+    {
+        // on end drag
+    }
+
+    private void OnPointerEnter(Card card)
+    {
+        // on pointer enter
+    }
+
+    private void OnPointerExit(Card card)
+    {
+        // on pointer exit
+    }
+
+    private void OnPointerUp(Card card, bool isOver)
+    {
+        // on pointer up
+    }
+
+    private void OnPointerDown(Card card)
+    {
+        // on pointer down
+    }
+
+    private void OnSelect(Card card, bool isSelected)
+    {
+        // on select
+    }
+
+
+}
