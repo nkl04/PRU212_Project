b01534e2ba0fea43a2113a22a034f08063ce5d3e
nkl04 <longnguyenkim2707@gmail.com>
2025-01-25 23:52:37 +0700

    feat: implement pointer event handlers

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeedLimit = 50f;
+
+    [Header("Selection")]
+    public bool isSelected = false;
+    public float selectionOffset = 50f;
+    private float pointerDownTime;
+    private float pointerUpTime;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
