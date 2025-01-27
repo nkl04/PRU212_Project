5642e5f0d4ea25bd607f0402bd0d74e7e2e9a3a3
nkl04 <longnguyenkim2707@gmail.com>
2025-01-28 01:03:41 +0700

    feat: add card SO

---
+++ Assets/Scripts/ScriptableObjects/CardInfo.cs
@@ -0,0 +1,50 @@
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card Info", menuName = "New Card")]
+public class CardInfo : ScriptableObject
+{
+    public Suit suit;
+    public Rank rank;
+    public int score;
+    public Sprite visual;
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        score = rank.GetHashCode();
+
+        int rankInt = rank == Rank.Ace ? 1 : rank.GetHashCode();
+
+        string spriteName = suit.ToString() + " " + rankInt.ToString();
+
+        Sprite sprite = AssetDatabase.LoadAssetAtPath($"Assets/Sprites/Deck/{spriteName}.png", typeof(Sprite)) as Sprite;
+
+        visual = sprite;
+    }
+#endif
+}
+public enum Suit
+{
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades
+}
+
+public enum Rank
+{
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+    Ace = 14
+}
